4346dcfafabde36aac8393540928053e4d899a67
ChristianBianchi99 <bianchi848@gmail.com>
2023-06-26 18:37:48 +0200

    Agg chiamata api

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import {store} from './data/store.js';
 import Header from './components/AppHeader.vue';
 import Main from './components/AppMain.vue';
+import axios from 'axios';
 export default {
   data(){
     return{
@@ -11,6 +12,14 @@ export default {
   components:{
     Header,
     Main
+  },
+  mounted(){
+    axios.get(store.api + 'movie?' + store.apiKey + '&query=' + store.searchedFilm).then((response) => {
+      store.filmsList = response.data.results;
+    }),
+    axios.get(store.api + 'tv?' + store.apiKey + '&query=' + store.searchedSeries).then((response) => {
+      store.seriesList = response.data.results;
+    })
   }
 }
 </script>
--- src/data/store.js
+++ src/data/store.js
@@ -1,5 +1,10 @@
 import {reactive} from 'vue';
 
 export const store = reactive({
-
+    api: 'https://api.themoviedb.org/3/search/',
+    apiKey: 'api_key=683e52e91baade4210b7478d0ea1c13d',
+    searchedFilm: '',
+    searchedSeries: '',
+    filmsList: '',
+    seriesList: '',
 })
